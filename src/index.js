d0588e82a0e01a65d4d6df2e79ac9a40247feca4
Alejo Gutierrez <alejox.gutierrez@gmail.com>
2021-07-14 17:57:11 -0500

    Skeleton React App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>It works!!!</h1>
+
+      </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
